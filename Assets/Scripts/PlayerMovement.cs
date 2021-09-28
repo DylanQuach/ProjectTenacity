8ac38674df0f985591f30cd6f53262353a34fbd3
Vincent <vicentelevy.a@gmail.com>
2021-09-28 14:53:37 -0600

    added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float Speed = 10.0f;
+
+    Rigidbody2D Rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody.AddForce(new Vector2(0, 500));
+        }
+    }
+    
+    //using this since it causes less stutter
+    private void FixedUpdate()
+    {
+        //move right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * +Speed * Time.deltaTime;
+        }
+
+        //move left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * -Speed * Time.deltaTime;
+        }
+
     }
 }
